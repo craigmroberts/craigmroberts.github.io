d95170ef1f2c0f18a06dce3e62353a7100a75376
Craig M Roberts <hello@craigmroberts.com>
2025-04-18 14:27:47 +0100

    Added scripts compiler

--- update-html.js
+++ update-html.js
@@ -6,9 +6,26 @@ const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 const version = packageJson.version;
 
 // Define the paths
+const distStylesDir = path.resolve(__dirname, 'dist/styles');
+const distScriptsDir = path.resolve(__dirname, 'dist/scripts');
 const htmlFilePath = path.resolve(__dirname, 'index.html');
 const cssFilePath = `./dist/styles/main.min.v${version}.css`;
 const jsFilePath = `./dist/scripts/main.min.v${version}.js`;
+
+// Helper function to delete old versioned files
+function deleteOldFiles(directory, filePrefix) {
+  const files = fs.readdirSync(directory);
+  files.forEach((file) => {
+    if (file.startsWith(filePrefix) && !file.endsWith(`v${version}.css`) && !file.endsWith(`v${version}.js`)) {
+      fs.unlinkSync(path.join(directory, file));
+      console.log(`Deleted old file: ${file}`);
+    }
+  });
+}
+
+// Delete old CSS and JS files
+deleteOldFiles(distStylesDir, 'main.min.v');
+deleteOldFiles(distScriptsDir, 'main.min.v');
 
 // Read the HTML file
 let htmlContent = fs.readFileSync(htmlFilePath, 'utf8');
